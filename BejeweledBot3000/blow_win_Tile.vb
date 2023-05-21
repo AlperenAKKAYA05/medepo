0e2b07f3f6b7dda39df7e1005e522e4104aef9e2
Alperen AKKAYA <38250510+AlperenAKKAYA05@users.noreply.github.com>
2023-05-21 07:16:32 +0300

    C
    C

--- BejeweledBot3000/blow_win_Tile.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class blow_win_Tile
-    Public TileCode As Integer
-    Public NormalisedTileCode As Integer
-
-    Public Sub New(tileCode As Integer)
-        Me.TileCode = tileCode
-    End Sub
-End Class
